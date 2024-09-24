a32d71c42effa112d82c5df5abd422e3ddd82ffe
Rakesh Patel <rakesh246808@gmail.com>
2024-09-24 22:29:17 +0530

    fixed some bugs

--- Components/HourContents.jsx
+++ Components/HourContents.jsx
@@ -8,20 +8,35 @@ function HourContents(props)
         <div className="time">
             <div className="time-content">
                 <h3 id="t11" className="fHour">
-                {props.fHr < 10 ? `0${props.fHr} : 59` : `${props.fHr} : 59`}
+                {props.fHr < 10 ? `0${props.fHr}:59` : `${props.fHr}:59`}
                 </h3>
                 <span className="time-format-up">{props.fTimeFrmt}</span>
             </div>
             <span>to</span>
             <div className="time-content">
                 <h3 id="t12" className="sHour">
-                {props.sHr < 10 ? `0${props.sHr} : 59` : `${props.sHr} : 59`}
+                {props.sHr < 10 ? `0${props.sHr}:59` : `${props.sHr}:59`}
                 </h3>
                 <span className="time-format-down">{props.sTimeFrmt}</span>
             </div>
             <span className="hour-temperatures">34°C</span>
         </div>
     </div>
+    // {/* <div className="hour-content">
+    //             <img src="./src/assets/gifs/heavyrain.gif" alt="Raining" />
+    //             <div className="time">
+    //               <div className="time-content">
+    //                 <h3 id="t11" className="up-time">11:59</h3>
+    //                 <span className="time-format-up">AM</span>
+    //               </div>
+    //               <span>to</span>
+    //               <div className="time-content">
+    //                 <h3 id="t12" className="down-time">12:59</h3>
+    //                 <span className="time-format-down">AM</span>
+    //               </div>
+    //               <span className="hour-temperatures">34°C</span>
+    //              </div>
+    //         </div> */}
    );
 }
 
