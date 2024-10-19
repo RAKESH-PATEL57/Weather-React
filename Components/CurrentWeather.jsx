579f0d2fc63f3311af0319d78ca773ec72f00126
Rakesh Patel <rakesh246808@gmail.com>
2024-10-19 18:11:58 +0530

    working on current weather

--- Components/CurrentWeather.jsx
+++ Components/CurrentWeather.jsx
@@ -1,5 +1,21 @@
-function CurrentWeather()
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line react/prop-types
+
+function CurrentWeather(props)
 {
+
+  
+  let date= new Date();
+
+  const day = date.getDate();
+  const month = date.toLocaleString('en-US', { month: 'short' });
+  const year = date.getFullYear();
+
+  const currentFullDate = `${day} ${month} ${year}`;
+
+ 
+
+
     return (
         <>
         <div className="search-section">
@@ -10,12 +26,17 @@ function CurrentWeather()
         <div className="current-location">
           <div className="place-report">
             <h1 className="currentPlace">At -<span id="currentPlaceName"> Sambalpuri</span></h1>
-            <h3 className="dt">Date - 1 Oct 2023</h3>
+            <h3 className="dt">Date - {currentFullDate}</h3>
             <h3 className="humidity">Humidity :- 50%</h3>
           </div>
           <div className="current-img-temp">
             <img className="current-img" src="/src/assets/gifs/heavyrain.gif" alt="cloudy" />
-            <h3 className="temp">37°C</h3>
+            <div className="currentTemp">
+             {props.loading === true ? <div className="loading"></div>:null}
+             
+             <h3 className="temp">{props.currentTemperature}°C</h3>
+              {/* <h3 className="temp">20°C</h3> */}
+            </div>
           </div>
         </div>
         </>
