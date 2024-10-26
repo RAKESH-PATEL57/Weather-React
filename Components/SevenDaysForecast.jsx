781b7f1a520c63e3dc823c2cdcb5acf57ca16397
Rakesh Patel <rakesh246808@gmail.com>
2024-10-26 17:05:37 +0530

    updated and added loading animations

--- Components/SevenDaysForecast.jsx
+++ Components/SevenDaysForecast.jsx
@@ -1,77 +1,112 @@
-import SevenDaysNamesWeather from "./SevenDaysNamesWeather";
-function SevenDaysForecast()
-{
-  // let sevenDaysList = document.querySelectorAll(".seven-days-list");
+/* eslint-disable react/prop-types */
+function SevenDaysForecast({ weatherData, loading }) {
   const date = new Date();
-  const weekDaysNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday','Saturday'];
-  
-  // console.log(currentWeekDayName);
-  
-  // let days = date.getDate();
-  
-  // day ra taa milu che i ra value setaa rakhmaa aau 7 din chalaama
-  // console.log(currentWeekDayName);
-  // sunday - 0
-  // monday - 1
-  // tuesday - 2
-  // wednesday - 3
-  // thursday - 4
-  // friday - 5
-  // saturday - 6
-  var daycount = 0;
-  var dayLoopStart = 0;
+  const weekDaysNames = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+  ];
 
-  function generatingDay(i)
-  {
-    
-    daycount = date.getDay() + i;
-    if(daycount > 6)
-      {
-        dayLoopStart++;
-        return weekDaysNames[dayLoopStart-1];
-      }
-      return weekDaysNames[daycount];
-    }
-    
-    let i=0;
+  // Generate day names for each day in the forecast
+  function generatingDay(i) {
+    return weekDaysNames[(date.getDay() + i) % 7];
+  }
 
+  // Show loading animation if data is still loading
+  if (loading) {
     return (
-        <div className="sevendays">
-        <h3>5 days forecast</h3>
-        <div className="forecastcontainer">
-          <SevenDaysNamesWeather 
-            generateDay = {generatingDay(i++)} 
-            imageLink = "/src/assets/gifs/thunder-storm.gif"
-          />
-          <SevenDaysNamesWeather 
-            generateDay = {generatingDay(i++)} 
-            imageLink = "/src/assets/gifs/heavyrain.gif"
-          />
-          <SevenDaysNamesWeather 
-            generateDay = {generatingDay(i++)} 
-            imageLink = "/src/assets/gifs/raininng.gif"
-          />
-          <SevenDaysNamesWeather 
-            generateDay = {generatingDay(i++)} 
-            imageLink = "/src/assets/gifs/sunandcloud2.gif"
-          />
-          <SevenDaysNamesWeather 
-            generateDay = {generatingDay(i++)} 
-            imageLink = "/src/assets/gifs/sunandcloud.gif"
-          />
-          <SevenDaysNamesWeather 
-            generateDay = {generatingDay(i++)} 
-            imageLink = "/src/assets/gifs/sunnyrain.gif"
-          />
-          <SevenDaysNamesWeather 
-            generateDay = {generatingDay(i++)} 
-            imageLink = "/src/assets/gifs/sunandcloud.gif"
-          />
-        
-        </div>  
+    <div className="sevendaysContainer">
+      <h3>7 Days Forecast</h3>
+      <div className="forecastcontainer">
+        {weatherData.temperature_2m_max.map((temp, index) => (
+          <div className="days"  key={index}>
+            <h3 className="seven-days-list">{generatingDay(index)}</h3>  {/* Displaying the day */}
+            <div className="sevenDaysTempContainer">
+               <div className="loading"></div>
+               <h4>{temp}°C</h4>                            {/* Displaying the temperature */}
+            </div>
+            <div className="sevenDaysWeatherImageContainer">
+               <div className="loading"></div>
+               <img src={getWeatherImage(weatherData.weathercode[index])} alt="weather-icon" />          {/* Displaying the image */}
+            </div>
+          </div>
+        ))}
       </div>
+    </div>
     );
+  }
+
+  return (
+    <div className="sevendaysContainer">
+    <h3>7 Days Forecast</h3>
+    <div className="forecastcontainer">
+      {weatherData.temperature_2m_max.map((temp, index) => (
+         <div className="days"  key={index}>
+          <h3 className="seven-days-list">{generatingDay(index)}</h3>                               {/* Displaying the day */}
+          <h4>{temp}°C</h4>                                                                         {/* Displaying the temperature */}
+          <img src={getWeatherImage(weatherData.weathercode[index])} alt="weather-icon" />          {/* Displaying the image */}
+      </div>
+      ))}
+    </div>
+  </div>
+  );
+}
+
+// Function to return an appropriate image based on the weather code
+function getWeatherImage(weatherCode) {
+  console.log(weatherCode);
+  switch (weatherCode) {
+    case 0: // Clear sky
+      return "/src/assets/gifs/clearSky.gif";
+
+    case 1: // Mainly clear
+    case 2: // Partly cloudy
+    case 3: // Overcast
+      return "/src/assets/gifs/partlyCloudy.gif";
+
+    case 45: // Fog
+    case 48: // Depositing rime fog
+      return "/src/assets/gifs/overCast.gif";
+
+    case 51: // Drizzle: Light intensity
+    case 53: // Drizzle: Moderate intensity
+    case 55: // Drizzle: Dense intensity
+      return "/src/assets/gifs/lighting.gif";
+
+    case 56: // Freezing Drizzle: Light intensity
+    case 57: // Freezing Drizzle: Dense intensity
+      return "/src/assets/gifs/snowFall.gif";
+
+    case 61: // Rain: Slight intensity
+    case 63: // Rain: Moderate intensity
+    case 65: // Rain: Heavy intensity
+      return "/src/assets/gifs/slightModerate.gif";
+
+    case 66: // Freezing Rain: Light intensity
+    case 67: // Freezing Rain: Heavy intensity
+    case 71: // Snow fall: Slight intensity
+    case 73: // Snow fall: Moderate intensity
+    case 75: // Snow fall: Heavy intensity
+    case 77: // Snow grains
+     return "/src/assets/gifs/snowFall.gif";
+
+    case 80: // Rain showers: Slight intensity
+    case 81: // Rain showers: Moderate intensity
+    case 82: // Rain showers: Violent intensity
+    return "/src/assets/gifs/heavyRain.gif";
+    
+    case 85: // Snow showers: Slight intensity
+    case 86: // Snow showers: Heavy intensity
+      return "/src/assets/gifs/snowFall.gif";
+
+    case 95: // Thunderstorm: Slight or moderate intensity
+      return "/src/assets/gifs/thunderstorm.gif";
+
+    case 96: // Thunderstorm with slight hail
+    case 99: // Thunderstorm with heavy hail
+      return "/src/assets/gifs/thunderStorm.gif";
+
+    default: // Default image for unknown or unspecified weather codes
+      return "/src/assets/gifs/default.gif";
+  }
 }
 
 export default SevenDaysForecast;
-
