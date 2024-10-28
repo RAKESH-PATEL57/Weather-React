a0230600ff7ff8eaf3ee02901f9d509047e6a61e
Rakesh Patel <rakesh246808@gmail.com>
2024-10-28 19:49:28 +0530

    changed Images on the basis of weatherCode

--- Components/HourContents.jsx
+++ Components/HourContents.jsx
@@ -1,49 +1,45 @@
+import GettingWeatherImage from "./GettingWeatherImage";
 /* eslint-disable react/prop-types */
-function HourContents(props)
-{
-    
-    let fTimeFormate = props.fHr < 24 ? "PM" :"AM";
-    let sTimeFormate = props.sHr <= 24 ? "PM" :"AM";
+function HourContents({ fHr, sHr, weatherCode, loading, temp }) {
+  const fTimeFormate = fHr >= 12 ? "PM" : "AM";
+  const sTimeFormate = sHr > 12 ? "PM" : "AM";
 
-    let upperTime = props.fHr % 12;
-    let lowerTime = props.sHr % 12;
-    
-   return (
+  const upperTime = fHr % 12 || 12; // Ensures 12-hour format displays 12 instead of 0
+  const lowerTime = sHr % 12 || 12;
+
+  // Get the weather image URL based on the weather code
+  const weatherImage = GettingWeatherImage(weatherCode);
+
+  return (
     <div className="hour-content">
-        <img src="/src/assets/gifs/heavyrain.gif" alt="Raining" />
-        <div className="time">
-            <div className="time-content">
-                <h3 id="t11" className="fHour">
-                {upperTime < 10 ? `0${upperTime}:00` : `${upperTime}:00`}
-                </h3>
-                <span className="time-format-up">{fTimeFormate}</span>
-            </div>
-            <span>to</span>
-            <div className="time-content">
-                <h3 id="t12" className="sHour">
-                {lowerTime < 10 ? `0${lowerTime}:59` : `${lowerTime}:59`}
-                </h3>
-                <span className="time-format-down">{sTimeFormate}</span>
-            </div>
-            <span className="hour-temperatures">34°C</span>
+      <div className="imageContainer">
+        {loading ? (
+          <div className="loading"></div>
+        ) : (
+          <img src={weatherImage} alt="Weather condition" />
+        )}
+      </div>
+      <div className="time">
+        <div className="time-content">
+          <h3 id="t11" className="fHour">
+            {upperTime < 10 ? `0${upperTime}:00` : `${upperTime}:00`}
+          </h3>
+          <span className="time-format-up">{fTimeFormate}</span>
         </div>
+        <span>to</span>
+        <div className="time-content">
+          <h3 id="t12" className="sHour">
+            {lowerTime < 10 ? `0${lowerTime}:59` : `${lowerTime}:59`}
+          </h3>
+          <span className="time-format-down">{sTimeFormate}</span>
+        </div>
+        <div className="temp-container">
+          {loading ? <div className="loading"></div> : null}
+          <span className="hour-temperatures">{temp}°C</span>
+        </div>
+      </div>
     </div>
-    // {/* <div className="hour-content">
-    //             <img src="./src/assets/gifs/heavyrain.gif" alt="Raining" />
-    //             <div className="time">
-    //               <div className="time-content">
-    //                 <h3 id="t11" className="up-time">11:59</h3>
-    //                 <span className="time-format-up">AM</span>
-    //               </div>
-    //               <span>to</span>
-    //               <div className="time-content">
-    //                 <h3 id="t12" className="down-time">12:59</h3>
-    //                 <span className="time-format-down">AM</span>
-    //               </div>
-    //               <span className="hour-temperatures">34°C</span>
-    //              </div>
-    //         </div> */}
-   );
+  );
 }
 
 export default HourContents;
