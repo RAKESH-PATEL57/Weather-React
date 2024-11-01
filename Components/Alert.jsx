db5899f7b19e64af422e285fb8f4017f97a21c23
Rakesh Patel <rakesh246808@gmail.com>
2024-11-01 16:40:01 +0530

    added a component for alert message

---
+++ Components/Alert.jsx
@@ -0,0 +1,11 @@
+function Alert()
+{
+    return (
+        <div className="alertMessage">
+           <h2>Please Enter Place Name to Know The Weather</h2>
+           <img src="../src/assets/gifs/rainingAlert.gif" alt="" />
+        </div>
+    )
+}
+
+export default Alert
